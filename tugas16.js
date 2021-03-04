c060c9c62b7663ab0725eea5b3e2da92009f0910
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-04 22:04:32 +0700

    Tugas 16 JS
    Tugas Niomic

---
+++ tugas16.js
@@ -0,0 +1,9 @@
+var deret1 = [45,100,99,3,2,4,1,2,3,19,22,14,10];
+console.log("Nilai Minimal : "+Math.min(...deret1));
+console.log("Nilai Maksimal : "+Math.max(...deret1));
+
+console.log("")
+
+var deret2 = [45,100,99,3,2,81,22,55,1,2,3,4,5];
+console.log("Nilai Minimal : "+Math.min(...deret2));
+console.log("Nilai Maksimal : "+Math.max(...deret2));
